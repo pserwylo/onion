2df7085728dd40e4decfa5f1d556052e32885cfe
Peter Serwylo <peter@serwylo.com>
2025-03-08 21:06:01 +1100

    Extract camera component to share between scene editor and project editor.

---
+++ src/components/Camera.tsx
@@ -0,0 +1,129 @@
+import Webcam from "react-webcam";
+import { useCallback, useRef, useState } from "react";
+import {
+  Box,
+  Button,
+  CircularProgress,
+  IconButton,
+  Tooltip,
+  Typography,
+} from "@mui/material";
+import { CameraAlt, Cameraswitch } from "@mui/icons-material";
+import clsx from "clsx";
+
+type IProps = {
+  onCapture: (image: string | null) => void;
+  overlay?: React.ReactNode;
+  actions?: React.ReactNode;
+};
+
+const Camera = ({ onCapture, overlay, actions }: IProps) => {
+  const [selfieCam, setSelfieCam] = useState(false);
+  const [webcamStatus, setWebcamStatus] = useState<
+    "initialising" | "connected" | "error"
+  >("initialising");
+
+  // https://github.com/mozmorris/react-webcam/issues/409#issuecomment-2404446979
+  const webcamRef = useRef<Webcam>(null);
+
+  const capture = useCallback(async () => {
+    const image = webcamRef.current?.getScreenshot();
+    onCapture(image ?? null);
+  }, [webcamRef]);
+
+  const reverse = () => {
+    setSelfieCam(!selfieCam);
+  };
+
+  const videoConstraints = {
+    width: 640,
+    facingMode: selfieCam ? "user" : "environment",
+  };
+
+  return (
+    <>
+      {webcamStatus === "error" && (
+        <Box className="flex flex-col py-24 px-12 text-center">
+          <Typography className="!mt-8 block">Unable to use webcam</Typography>
+          <Button
+            variant="text"
+            size="small"
+            onClick={() => setSelfieCam(!selfieCam)}
+            startIcon={<Cameraswitch />}
+          >
+            Try different webcam
+          </Button>
+        </Box>
+      )}
+
+      {webcamStatus === "initialising" && (
+        <Box className="flex flex-col py-24 px-12 text-center">
+          <Box className="flex gap-4 justify-center items-center">
+            <CircularProgress />
+            <Typography>Initialising webcam</Typography>
+          </Box>
+          <Typography className="!mt-8 block">Taking too long?</Typography>
+          <Button
+            variant="text"
+            size="small"
+            onClick={() => setSelfieCam(!selfieCam)}
+            startIcon={<Cameraswitch />}
+          >
+            Try different webcam
+          </Button>
+        </Box>
+      )}
+
+      <div className="camera-wrapper">
+        <Webcam
+          ref={webcamRef}
+          audio={false}
+          screenshotFormat="image/webp"
+          onUserMediaError={() => setWebcamStatus("error")}
+          onUserMedia={() => setWebcamStatus("connected")}
+          width={640}
+          disablePictureInPicture
+          className={clsx("camera camera--feed", {
+            hidden: webcamStatus !== "connected",
+          })}
+          videoConstraints={videoConstraints}
+        />
+
+        {webcamStatus === "connected" && (
+          <>
+            {overlay && (
+              <Box className="absolute max-w-full max-h-full inset-0">
+                {overlay}
+              </Box>
+            )}
+
+            <Box className="absolute flex top-4 right-4 p-1 font-md">
+              {actions}
+              <Tooltip title="Switch camera">
+                <IconButton onClick={reverse}>
+                  <Cameraswitch />
+                </IconButton>
+              </Tooltip>
+            </Box>
+          </>
+        )}
+      </div>
+
+      {webcamStatus === "connected" && (
+        <div className="mt-2 mb-4">
+          <Button
+            onClick={capture}
+            startIcon={<CameraAlt />}
+            variant="contained"
+            className="w-full py-4"
+            size="large"
+          >
+            Take Picture
+          </Button>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Camera;
--- src/project/ScenePhotoEditor.tsx
+++ src/project/ScenePhotoEditor.tsx
@@ -2,18 +2,10 @@ import { Link, useNavigate, useParams } from "react-router";
 import { addSceneImage, loadProject, selectScenes } from "./projectSlice.ts";
 import { useAppDispatch } from "../store/hooks.ts";
 import { useSelector } from "react-redux";
-import {
-  Box,
-  Button,
-  Container,
-  IconButton,
-  Tooltip,
-  Typography,
-} from "@mui/material";
-import { CameraAlt, Cameraswitch, ChevronLeft } from "@mui/icons-material";
-import { useCallback, useEffect, useRef, useState } from "react";
-import Webcam from "react-webcam";
-import clsx from "clsx";
+import { Button, Container, Typography } from "@mui/material";
+import { useCallback, useEffect } from "react";
+import Camera from "../components/Camera.tsx";
+import { ChevronLeft } from "@mui/icons-material";
 
 const ScenePhotoEditor = () => {
   const dispatch = useAppDispatch();
@@ -23,18 +15,6 @@ const ScenePhotoEditor = () => {
     sceneId: string;
   }>();
   const allScenes = useSelector(selectScenes);
-  const [selfieCam, setSelfieCam] = useState(false);
-  const [webcamStatus, setWebcamStatus] = useState<
-    "initialising" | "connected" | "error"
-  >("initialising");
-
-  // https://github.com/mozmorris/react-webcam/issues/409#issuecomment-2404446979
-  const webcamRef = useRef<Webcam>(null);
-
-  const videoConstraints = {
-    width: 640,
-    facingMode: selfieCam ? "user" : "environment",
-  };
 
   useEffect(() => {
     if (projectId) {
@@ -42,19 +22,17 @@ const ScenePhotoEditor = () => {
     }
   }, [dispatch, projectId]);
 
-  const capture = useCallback(async () => {
-    if (sceneId == null) {
-      return;
-    }
+  const capture = useCallback(
+    async (image: string | null) => {
+      if (sceneId == null || image == null) {
+        return;
+      }
 
-    const image = webcamRef.current?.getScreenshot();
-    if (!image) {
-      return;
-    }
-
-    await dispatch(addSceneImage({ sceneId, image }));
-    navigate(`/project/${projectId}/storyboard`);
-  }, [dispatch, sceneId, webcamRef]);
+      await dispatch(addSceneImage({ sceneId, image }));
+      navigate(`/project/${projectId}/storyboard`);
+    },
+    [navigate, dispatch, sceneId, projectId],
+  );
 
   if (sceneId == null) {
     console.warn("No scene ID provided to FrameEditor URL.");
@@ -78,71 +56,20 @@ const ScenePhotoEditor = () => {
     return null;
   }
 
-  const reverse = () => {
-    setSelfieCam(!selfieCam);
-  };
-
   return (
     <Container maxWidth="sm" className="flex flex-col gap-y-2">
       <Typography variant="h1">Animation Time</Typography>
       <Typography variant="h2">Scene {sceneIndex + 1}</Typography>
-      <div className="camera-wrapper">
-        <Webcam
-          ref={webcamRef}
-          audio={false}
-          screenshotFormat="image/webp"
-          onUserMediaError={() => setWebcamStatus("error")}
-          onUserMedia={() => setWebcamStatus("connected")}
-          width={640}
-          disablePictureInPicture
-          className={clsx("camera camera--feed", {
-            hidden: webcamStatus !== "connected",
-          })}
-          videoConstraints={videoConstraints}
-        />
-
-        {webcamStatus === "connected" && (
-          <>
-            <Box
-              sx={{
-                padding: "0.1em 0.2em",
-                fontSize: "1.5em",
-                position: "absolute",
-                display: "flex",
-                top: "1em",
-                right: "1em",
-              }}
-            >
-              <Tooltip title="Switch camera">
-                <IconButton onClick={reverse}>
-                  <Cameraswitch />
-                </IconButton>
-              </Tooltip>
-            </Box>
-          </>
-        )}
-      </div>
-      <Box className="flex gap-4">
-        <Button
-          component={Link}
-          to={`/project/${projectId}/storyboard`}
-          className="w-full"
-          startIcon={<ChevronLeft />}
-          variant="contained"
-        >
-          Back
-        </Button>
-        <Button
-          component={Link}
-          to={capture}
-          className="w-full"
-          startIcon={<CameraAlt />}
-          variant="contained"
-          onClick={capture}
-        >
-          Take Picture of Scene {sceneIndex + 1}
-        </Button>
-      </Box>
+      <Camera onCapture={capture} />
+      <Button
+        startIcon={<ChevronLeft />}
+        component={Link}
+        variant="outlined"
+        size="large"
+        to={`/project/${projectId}`}
+      >
+        Back
+      </Button>
     </Container>
   );
 };
